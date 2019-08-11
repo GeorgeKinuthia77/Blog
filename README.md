baf7cc9eaf11f476647184cb41af87e28d56b40b
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-11 22:16:19 +0300

    added description and author in my readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Blog
+
+## Description
+This is a web application that allows users to express themselves using a blog posts. They they first create an account then log in to start creating blogs..
+## Author
+George Njoroge(11/8/2019)
--- config.py
+++ config.py
@@ -1,32 +1,24 @@
-
 import os
 
 class Config:
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADS_DEFAULT_DEST ='app/static'
+    SECRET_KEY = 'ptex'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 588
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    # simple mde  configurations
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
-        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
+    pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
+    DEBUG = True
+
+class TestConfig(Config):
+
     DEBUG = True
 
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'production': ProdConfig,
+    'development':DevConfig,
+    'tests': TestConfig
 }
