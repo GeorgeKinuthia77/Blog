e8513292e20edfe3eb6ec59a624187f08e6a4f51
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-11 21:38:15 +0300

    changes in config.py

--- config.py
+++ config.py
@@ -1,24 +1,32 @@
+
 import os
 
 class Config:
-    SECRET_KEY = 'ptex'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/blog'
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADS_DEFAULT_DEST ='app/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
 
 class DevConfig(Config):
-    DEBUG = True
-
-class TestConfig(Config):
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
     DEBUG = True
 
 config_options = {
-    'production': ProdConfig,
-    'development':DevConfig,
-    'tests': TestConfig
+'development':DevConfig,
+'production':ProdConfig
 }
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export DATABASE_URI='postgresql+psycopg2://george:PTEXPTEX61@localhost/blog'
+export DATABASE_URI='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
 export SECRET_KEY='ptex'
 python3.6 manage.py server
