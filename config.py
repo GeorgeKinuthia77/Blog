a89726f8bcb32b9982817b2c55da727070822a61
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-13 11:36:52 +0300

    changes

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    
     SECRET_KEY = 'ptex'
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-export DATABASE_URI='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
 export SECRET_KEY='ptex'
+export SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
+
 python3.6 manage.py server
