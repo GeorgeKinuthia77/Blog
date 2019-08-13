1f75c1b7352c5ed798fe3996d1121b96354e451a
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-13 12:54:51 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,16 +1,20 @@
 import os
 
 class Config:
-    
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
     SECRET_KEY = 'ptex'
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
-    pass
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://george:PTEXPTEX61@localhost/vlog'
